bfaee18b1c36c3508e03028469f81cc13ce37cef
oryza valendio <107356587+JustOryza@users.noreply.github.com>
2025-09-17 12:07:15 +0700

    Add files via upload

---
+++ week5.py
@@ -0,0 +1,77 @@
+# # For dengan list
+# listKota = ['Jakarta', 'Kuala Lumpur', 'Manila', 'Bangkok', 'Hongkong', 'Tokyo', 'Seoul', 'Busan']
+# for kota in listKota:
+#     print(kota)
+
+# # Iterasi for dengan list
+# listKota = ['Jakarta', 'Kuala Lumpur', 'Manila', 'Bangkok', 'Hongkong', 'Tokyo', 'Seoul', 'Busan']
+# for i, kota in enumerate(listKota):
+#     print(i, kota)
+
+# # for dengan fungsi range
+# for n in range (5):
+#     print('Perulangan ke -', n)
+
+# # Perulangan bilangan ganjil
+# for i in range (1,9,2):
+#     print('loop ke -', i)
+
+## For dengan tuple 
+# tupleAlsin = ('Kubota', 'Yanmar', 'New Holand', 'Iseki')
+# for alsin in tupleAlsin:
+#     print(alsin)
+
+# # For dengan string
+# for karakter in "pertanian":
+#     print(karakter)
+
+# # Break dan continue pada looping
+# for i in range (1,5):
+#     if (i==3):
+#         continue
+#     print(i)
+
+# for i in range (1,5):
+#     if (i==4):
+#         break
+#     print(i)
+
+# # For else
+# listAlsin = ['Kubota', 'Yanmar', 'New Holand', 'Iseki']
+# for alsin in listAlsin:
+#     print(alsin)
+# else :
+#     print('Semua merk sudah dicetak')
+
+# listkota = ['Jakarta', 'Kuala Lumpur', 'Manila', 'Bangkok', 'Hongkong', 'Tokyo', 'Seoul', 'Busan']
+# carikota = str(input("Masukkan nama kota : "))
+# for i, kota in enumerate(listkota):
+#     if kota.lower() == carikota.lower():
+#         print('Kota yang anda cari berada di index : ', i)
+#         break
+# else:
+#   print('tidak ada')
+
+# Perulangan dengan while
+# while (4+2==6):
+#     print('Stackoverflow')
+
+# while (4+3==6):
+#     print('Stackoverflow')
+
+# a = int(input('Bilangan ganjil kurang dari 20'))
+# while a%2 != 0 and a<= 20:
+#     a = int(input('Salah, masukkan lagi ; '))
+# print("Benar")
+
+# Perulangan bersarang
+# for i in range (2):
+#     print('Perulangan luar [i] = ', i)
+# for j in range (3):
+#     print('Perulangan dalam [i, j] = ', str(i) + ', ' +str(j))
+
+# latihan progam modul 5
+n = int(input('Masukkan jumlah bilangan yang akan dipangkatkan : '))
+for :
+    print(f'Penjumlahan dari {n} adalah {i}')
+    i+=i                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
