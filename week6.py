d3e32733b63373db1d05d8726cfb3dd41feea0a7
oryza valendio <107356587+JustOryza@users.noreply.github.com>
2025-09-24 12:18:50 +0700

    Add files via upload

---
+++ week6.py
@@ -0,0 +1,79 @@
+# # user defined function
+# def halo_dunia():
+#     print('Hallo dunia!')
+# halo_dunia()
+
+# # Parameter wajib
+# def perkenalan (nama, asal):
+#     print(f'Perkenalkan saya {nama} berasal dari {asal}')
+# perkenalan('Abdul','Ngawi')
+
+# # Parameter opsional
+# def suhu_udara (daerah, derajat, satuan = 'celcius'):
+#     print(f'suhu di daerah {daerah} adalah {derajat} {satuan}')
+
+# suhu_udara('Surabaya', 30)
+# suhu_udara('Surabaya', 89, 'fahrenheit')
+
+# def suhu_udara (daerah, derajat = 30, satuan = 'celcius'):
+#     print(f'suhu di daerah {daerah} adalah {derajat} {satuan}')
+# suhu_udara('jakarta', 'fahrenheit')
+# suhu_udara('jakarta', satuan='fahrenheit')
+# suhu_udara(satuan='kelvin', daerah='makasar', derajat=100)
+
+# Fungsi yang mengembalikan nilai
+# def luas_persegi(sisi):
+#     return sisi*sisi
+
+# luas_persegi(100)
+# print(f'luas persegi dengan sisi 4 adalah {luas_persegi(4)}')
+# persegi_besar = luas_persegi(100)
+# persegi_kecil = luas_persegi(50)
+
+# print(f'total luas persegi besar dan kecil adalah {persegi_besar + persegi_kecil} ')
+
+# def persentase (total, jumlah):
+#     if (total >= 0 and total <= jumlah):
+#         return total / jumlah * 100
+#     return False
+# print(persentase(30, 60))
+# print(persentase(100, 60))
+
+# kota = 'bogor'
+# def halo():
+#     print(kota)
+
+# print(f'[print secara langsung] {kota}')
+# print('Panggil fungsi halo ', end='')
+# halo()
+
+# kota, provinsi = 'Bogor', 'Jawa barat'
+# def hello():
+#     provinsi = 'Jawa Timur'
+#     print(kota, provinsi)
+# print('Panggil fungsi hello()')
+# hello()
+# print('\n [Secara Langsung]')
+# print(kota, provinsi)
+
+# def tampilAngka (batas, i=1):
+#     print(f'Perulangan ke {i}')
+
+#     if (i < batas):
+#         tampilAngka(batas, i+1)
+
+# tampilAngka(5)
+
+# def tampilAngka (batas, i=1):
+#     if (i < batas):
+#         tampilAngka(batas, i+1)
+#     print(f'Perulangan ke {i}')
+# tampilAngka(5)
+
+def fn_recursive (nilai, i=2):
+    if (i < nilai):
+        fn_recursive(nilai, i+1)
+    x = nilai * (i-1)
+    # print(f'{i} * {i-1}')
+    print(x)
+fn_recursive(5)
